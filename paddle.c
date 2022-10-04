36f74deae4c5677b32f96e50d1b1b98348eb5d1c
dannyjb164 <dannyjb164@gmail.com>
2022-10-05 11:40:02 +1300

    More updates to paddle. Can be left until other functions updated.

--- paddle.c
+++ paddle.c
@@ -1,45 +1,58 @@
+#include "paddle.h"
 #include "tinygl.h"
 #include "pacer.h"
 #include "navswitch.h"
+#include "game.h"
 
 #include <stdint.h>
 #include <stdbool.h>
 
-#define LEFT_MAX 0
-#define RIGHT_MAX 5
-#define ROW 1
-
-
-/* The position of the left of the paddle, with width two */
-static uint8_t col = 1;
-
 /*
  * Initialises the paddle
  */
-void paddle_init(void)
+void paddle_init (void)
 {
-
+    paddle.left = START_LEFT;
+    paddle.right = START_RIGHT;
+    paddle_draw();
 }
 
 /*
  * Moves the paddle by incrementing or decrementing the col value
  */
-void paddle_move(void)
+void paddle_move (void)
 {
-    if (navswitch_push_event_p (NAVSWITCH_SOUTH) && col != LEFT_MAX)
-        col --;
+    if (navswitch_push_event_p (NAVSWITCH_SOUTH) && paddle.left != LEFT_MAX) {
+        paddle.left--;
+        paddle.right--;
+    }
 
-    if (navswitch_push_event_p (NAVSWITCH_NORTH) && col != RIGHT_MAX)
-        col++;
-
+    if (navswitch_push_event_p (NAVSWITCH_NORTH) && paddle.right != RIGHT_MAX) {
+        paddle.left++;
+        paddle.right++;
+    }
 }
 
 /*
  * Draws the paddle using the tinygl files
  */
-void paddle_draw(void)
+void paddle_draw (void)
 {
-    tinygl_point_t left = tinygl_point(ROW, col);
-    tinygl_point_t right = tinygl_point(ROW, col+1);
+    tinygl_point_t left = tinygl_point(PADDLE_ROW, paddle.left);
+    tinygl_point_t right = tinygl_point(PADDLE_ROW, paddle.right);
     tinygl_draw_line(left, right, true);
+}
+
+/*
+ * Checks to see if the ball is colliding with the paddle
+ * @param the ball in the game
+ * @return boolean whether the ball collides
+ */
+bool ball_on_paddle (Ball_t ball)
+{
+    if (ball.row == PADDLE_ROW) {
+        if (ball.col == paddle.left || ball.col == paddle.right)
+            return true;
+    }
+    return false;
 }
--- paddle.h
+++ paddle.h
@@ -1,6 +1,38 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "game.h"
 
+typedef struct
+{
+    uint8_t left;
+    uint8_t right;
+} paddle_t;
+
+paddle_t paddle;
+
+/*
+ * Initialises the paddle
+ */
+void paddle_int (void);
+
+/*
+ * Moves the paddle by incrementing or decrementing the col value
+ */
+void paddle_move (void);
+
+/*
+ * Draws the paddle using the tinygl files
+ */
+void paddle_draw (void);
+
+/*
+ * Checks to see if the ball is colliding with the paddle
+ * @param the ball in the game
+ * @return boolean whether the ball collides
+ */
+bool ball_on_paddle (Ball_t ball);
 
 #endif
