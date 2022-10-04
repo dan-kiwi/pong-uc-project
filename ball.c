64e61eec142a29e11bb2b3ef532711339307e29c
Bethany <bakb@hotmail.co.nz>
2022-10-04 16:39:38 +1300

    ball stuff

--- ball.c
+++ ball.c
@@ -9,14 +9,21 @@ void ball_init(void)
 void ball_move_forward(//ball)
 {
     ball.row++;
+    
     if (ball.right) {
         ball.column++;
+        
+        //ball is at full right side of the board so direction must be flipped
         if (ball.column >= 6) {
             ball.right == false;
         }
     } else {
         ball.column--;
-        if (ball.column)
+        
+        //ball is at full left side of the board so direction must be flipped
+        if (ball.column <= 0) {
+            ball.right == true;
+        }
     }
     
 }
