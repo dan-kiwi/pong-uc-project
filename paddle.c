577b1d0cf6690b8771bab365ddc05d2e51e9a849
dannyjb164 <dannyjb164@gmail.com>
2022-10-04 17:13:04 +1300

    Changes to paddle

--- paddle.c
+++ paddle.c
@@ -1,21 +1,26 @@
 #include "tinygl.h"
 #include "pacer.h"
+#include "navswitch.h"
 
 #include <stdint.h>
 
+#define LEFT_MAX 0
+#define RIGHT_MAX 5
+
+/* The position of the left of the paddle, with width two */
 static uint8_t col = 1;
 
 void paddle_init(void)
 {
-
+    t
 }
 
 void paddle_move(int8_t movement)
 {
-    if (movement < 0 && col > 0) {
+    if (navswitch_push_event_p (NAVSWITCH_SOUTH) && col != LEFT_MAX)
         col --;
-    }
-    if (movement > 0 && col < 5) {
+
+    if (navswitch_push_event_p (NAVSWITCH_NORTH) && col != RIGHT_MAX)
         col++;
-    }
+
 }
