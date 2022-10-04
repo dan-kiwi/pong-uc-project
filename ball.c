00b3a237cccff58221efdda0b1c343cd413cb7e5
Bethany <bakb@hotmail.co.nz>
2022-10-04 16:56:05 +1300

    ball stuff

--- ball.c
+++ ball.c
@@ -36,17 +36,32 @@ void ball_move_back(//ball)
         ball.column--
         
         //ball is at full right side of the board so direction must be flipped
-        if (ball.column <= 0) {
+        if (ball.column <= 0)
             ball.right == false;
-        }
         
     } else {
         ball.column++;
         
         //ball is at full left side of the board so direction must be flipped
-        if (ball.column >= 6) {
+        if (ball.column >= 6)
             ball.right == true;
-        }
         
     }
+}
+
+bool ball_at_paddle(ball)
+{
+    bool at_paddle = false;
+    if (ball.row == 0) {
+        at_paddle = true;
+    }
+    return at_paddle;
+}
+
+bool ball_to_partner(ball)
+{
+    bool to_partner = false;
+    if (ball.row == 4)
+        to_partner = true;
+    return to_partner;
 }
