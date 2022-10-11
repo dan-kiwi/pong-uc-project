c435973fbed9b5f15c0e3e51978067b0da7c8f4e
Bethany Kaye-Blake <bak55@uclive.ac.nz>
2022-10-11 02:28:15 +0000

    Updating win.h as per win.c

--- win.h
+++ win.h
@@ -10,4 +10,23 @@
 
 #include <stdio.h>
 
+/*
+ * Uses the location of the ball and paddle to determine a loss
+ * If the ball is at the bounce row but not the paddle the player has lost
+ * and so the func returns true
+ */
+bool check_if_lost(Ball_t* ball, Paddle_t paddle);
+
+
+/*
+ * Sends the character "L" if player has lost
+ */
+void send_loss();
+
+
+/*
+ *Checks to see if other player has lost, amd hence this player has won
+ */
+bool receive_loss();
+
 #endif /* win_h */
