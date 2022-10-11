84aa28f7eecfc1ca3763b23ea1dafe87572c5da1
Bethany Kaye-Blake <bak55@uclive.ac.nz>
2022-10-11 02:56:47 +0000

    updating game with win/lose stuff

--- game.c
+++ game.c
@@ -17,6 +17,8 @@ uint16_t to_count = 100;
 Ball_t ball;
 Paddle_t paddle;
 bool player1;
+bool win = false;
+bool game_end = false;
 
 int main (void)
 {
@@ -48,14 +50,32 @@ int main (void)
                 to_count--;
             }
             ball_move(&ball, paddle);
+
             if (check_ball(&ball)) {
                 change_player(&player1);
                 send_ball(&ball);
+            } else {
+                if (check_if_lost(&ball, paddle)) {
+                    send_loss();
+                    game_end = true;
+                }
             }
         }
-
+        if (receive_loss()) {
+            win == true;
+            game_end = true;
+        }
         tinygl_update ();
         counter++;
         check_player(&player1, &ball);
+
+
+        //to start a new game
+        if (game_end) {
+            game_end = false;
+            
+            repeat_game_screen();
+            set_player(&player1);
+        }
     }
 }
